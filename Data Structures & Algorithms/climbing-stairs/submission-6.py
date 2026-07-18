c1cb863838b2ccf9be9914bf22f5ba5d43e31258
Fernando <75755777+fcallerafilho@users.noreply.github.com>
2026-07-18 13:50:20 -0300

    Add: climbing-stairs - submission-6

---
+++ Data Structures & Algorithms/climbing-stairs/submission-6.py
@@ -0,0 +1,18 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 2:
+            return n
+
+        dp = [1, 2]
+        i = 3
+
+        while i <= n:
+            aux = dp[1]
+            dp[1] = dp[1] + dp[0]
+            dp[0] = aux
+            i+=1
+
+        return dp[1]
+
+
+        
