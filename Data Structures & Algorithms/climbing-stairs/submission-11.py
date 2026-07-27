0f030d50d66333a9a5e97ecbd9cfd3593ee17923
Fernando <75755777+fcallerafilho@users.noreply.github.com>
2026-07-27 14:13:30 -0300

    Add: climbing-stairs - submission-11

---
+++ Data Structures & Algorithms/climbing-stairs/submission-11.py
@@ -0,0 +1,11 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        dp = [0] * (n+1)
+        dp[-1] = 1
+
+        for i in range(n-1, -1, -1):
+            dp[i] += dp[i+1]
+            if i + 2 <= n:
+                dp[i] += dp[i+2]
+
+        return dp[0]
