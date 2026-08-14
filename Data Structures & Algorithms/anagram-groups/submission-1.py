c8f56b6089bf1741f951a6e0f13d56c1a7ce91eb
Fernando <75755777+fcallerafilho@users.noreply.github.com>
2026-08-13 21:27:05 -0300

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        strs_map = {} # char count list : list of strings with it
+        for s in strs:
+            chars = [0] * 26
+            for c in s:
+                chars[ord(c) - ord('a')] += 1
+
+            if tuple(chars) not in strs_map:
+                strs_map[tuple(chars)] = []
+            strs_map[tuple(chars)].append(s)
+
+        res = [list(val) for val in strs_map.values()]
+        return res
+
+            
+
+
