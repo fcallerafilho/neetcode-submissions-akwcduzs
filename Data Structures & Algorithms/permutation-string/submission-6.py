c0c98c07b92f0c312fb72cefeee5624ee159b606
Fernando <75755777+fcallerafilho@users.noreply.github.com>
2026-08-15 20:19:55 -0300

    Add: permutation-string - submission-6

---
+++ Data Structures & Algorithms/permutation-string/submission-6.py
@@ -0,0 +1,20 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+
+        s1_map = {}
+
+        for c in s1:
+            s1_map[c] = s1_map.get(c, 0) + 1
+
+        for i in range(len(s2) - len(s1) + 1):
+            s2_map = {}
+            for j in range(i, i+len(s1)):
+                if s2[j] not in s1_map:
+                    break
+                s2_map[s2[j]] = s2_map.get(s2[j], 0) + 1
+                if s2_map == s1_map:
+                    return True
+
+        return False
